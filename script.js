a99ee9ca26e07611a2f95646a2f4fc21c040dac5
Tommy <TommyCohn116@gmail.com>
2017-03-29 21:36:35 -0400

    More code brought in from older projects.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // CONSTANTS
 //----------------------------------------------------------------------------------------------------
 
-
+var mathSpecialStrings = ["(", ")", "+", "-", "*", "/", "^", "arcsin", "arccos", "arctan", "cos", "sin", "tan", "cot", "sec", "csc", "sqrt", "log", "ln", "PI", "E", "T", ","];
 
 //----------------------------------------------------------------------------------------------------
 // GLOBAL VARIABLES
@@ -59,6 +59,63 @@ function rawFuncStringToArray(str) {
 
 	var fArray = [];
 
+	var currentString = str;
+	var nextChar;
+	var num = "";
+	var lastCharOperator = true;
+	var negative;
+	var currentCharIndex = 0;
+	while(currentString.length > 0) {
+		while(currentString[0] == " " && currentString.length > 0) {
+			currentString = currentString.substr(1);
+			++currentCharIndex;
+		}
+		if(currentString.length == 0) {
+			break;
+		}
+		nextChar = false;
+		negative = false;
+		if(lastCharOperator && currentString[0] == "-" && !isNaN(currentString[1])) {
+			negative = true;
+		}
+		if(!negative) {
+			for(var i=0; i<mathSpecialStrings.length; ++i) {
+				if(0 == currentString.indexOf(mathSpecialStrings[i])) {
+					fArray.push(mathSpecialStrings[i]);
+					currentString = currentString.substr(mathSpecialStrings[i].length);
+					currentCharIndex += mathSpecialStrings[i].length;
+					nextChar = true;
+					if(!(mathSpecialStrings[i] == "(" || mathSpecialStrings[i] == ")")) {
+						lastCharOperator = true;
+					}
+					break;
+				}
+			}
+		}
+		if(!nextChar) {
+			lastCharOperator = false;
+			num = "";
+			if(currentString[0] == "-") {
+				num += "-";
+				currentString = currentString.substr(1);
+				++currentCharIndex;
+			}
+			if(currentString[0] == ".") {
+				num += "0";
+				currentString = currentString.substr(1);
+				++currentCharIndex;
+			}
+			if(!isOperand(currentString[0])) {
+				throw(currentCharIndex);
+			}
+			while(!isNaN(currentString[0]) || currentString[0] == ".") {
+				num = num + currentString[0];
+				currentString = currentString.substr(1);
+				++currentCharIndex;
+			}
+			fArray.push(num);
+		}
+	}
 	return fArray;
 }
 function differentiate(func) {
@@ -75,8 +132,28 @@ function parseToImgURL(d) {
 
 	return url;
 }
+function isOperand(char) {
+	console.log("isOperand(" + char + ")");
+	
+	if(!isNaN(Number(char))) {
+		return true;
+	}
+	else if(char == "PI") {
+		return true;
+	}
+	else if(char == "E") {
+		return true;
+	}
+	else if(char == "T") {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
 //----------------------------------------------------------------------------------------------------
 // EXECUTED CODE
 //----------------------------------------------------------------------------------------------------
 
+setup();
