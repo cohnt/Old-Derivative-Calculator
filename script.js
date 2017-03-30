bf2be6b0ce473a8333f753c734ad4383ea298352
Tommy <TommyCohn116@gmail.com>
2017-03-29 21:12:55 -0400

    Some basic program structure.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // GLOBAL VARIABLES
 //----------------------------------------------------------------------------------------------------
 
-
+var page = {};
 
 //----------------------------------------------------------------------------------------------------
 // CLASSES
@@ -20,7 +20,28 @@
 // FUNCTIONS
 //----------------------------------------------------------------------------------------------------
 
+function setup() {
+	console.log("FUNCTION CALL: setup()");
 
+	page.userFunc = document.getElementById("userFunc"); //
+	page.calcButton = document.getElementById("calcButton");
+	page.solution = document.getElementById("solution");
+
+	page.calcButton.addEventListener("click", solve);
+}
+function solve() {
+	console.log("FUNCTION CALL: solve()");
+
+	var rawFuncString = page.userFunc.value;
+	var funcArray = rawFuncStringToArray(rawFuncString);
+}
+function rawFuncStringToArray(str) {
+	console.log("FUNCTION CALL: rawFuncStringToArray(" + str + ")");
+
+	var fArray = [];
+
+	return fArray;
+}
 
 //----------------------------------------------------------------------------------------------------
 // EXECUTED CODE
