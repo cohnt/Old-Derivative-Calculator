a8b3f8dedaa56b31c522b28bfee707c4244d7951
Tommy <TommyCohn116@gmail.com>
2017-04-07 13:12:46 -0400

    Clean away subtraction, to replace it with product of -1 and the second number.

--- script.js
+++ script.js
@@ -209,6 +209,15 @@ function rawFuncStringToArray(str) {
 			fArray.push(num);
 		}
 	}
+
+	for(var i=0; i<fArray.length; ++i) {
+		if(fArray[i] == "-") {
+			fArray.splice(i, 1, "+");
+			fArray.splice(i+1, 0, "*");
+			fArray.splice(i+2, 0, "-1");
+		}
+	}
+
 	return fArray;
 }
 function makeStackTree(pf) {
